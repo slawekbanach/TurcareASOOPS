82d927ebd74fc6e47d935ede47e2589991b633eb
Slawomir Banach <slawomib@student.hist.no>
2016-02-23 13:09:06 +0100

    registrering av bruker

--- TurCareASOOPS/TurCareASOOPS/RegistrerBruker.Designer.vb
+++ TurCareASOOPS/TurCareASOOPS/RegistrerBruker.Designer.vb
@@ -22,8 +22,134 @@ Partial Class RegistrerBruker
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtRegistrerbrukernavn = New System.Windows.Forms.TextBox()
+        Me.txtregistrerpw = New System.Windows.Forms.TextBox()
+        Me.txtgjentapw = New System.Windows.Forms.TextBox()
+        Me.ComboAnsattype = New System.Windows.Forms.ComboBox()
+        Me.btnRegistrerBruker = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(96, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Registrer en ny bruker:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(40, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(62, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Brukernavn"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(40, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Passord"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(40, 161)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Gjenta passord"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(40, 201)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Ansatt-type"
+        '
+        'txtRegistrerbrukernavn
+        '
+        Me.txtRegistrerbrukernavn.Location = New System.Drawing.Point(153, 79)
+        Me.txtRegistrerbrukernavn.Name = "txtRegistrerbrukernavn"
+        Me.txtRegistrerbrukernavn.Size = New System.Drawing.Size(122, 20)
+        Me.txtRegistrerbrukernavn.TabIndex = 5
+        '
+        'txtregistrerpw
+        '
+        Me.txtregistrerpw.Location = New System.Drawing.Point(153, 117)
+        Me.txtregistrerpw.Name = "txtregistrerpw"
+        Me.txtregistrerpw.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtregistrerpw.Size = New System.Drawing.Size(122, 20)
+        Me.txtregistrerpw.TabIndex = 6
+        '
+        'txtgjentapw
+        '
+        Me.txtgjentapw.Location = New System.Drawing.Point(153, 158)
+        Me.txtgjentapw.Name = "txtgjentapw"
+        Me.txtgjentapw.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtgjentapw.Size = New System.Drawing.Size(122, 20)
+        Me.txtgjentapw.TabIndex = 7
+        '
+        'ComboAnsattype
+        '
+        Me.ComboAnsattype.FormattingEnabled = True
+        Me.ComboAnsattype.Items.AddRange(New Object() {"Selger", "Lagerarbeider", "Kursleder", "Daglig leder ", "Sekretær", "admin"})
+        Me.ComboAnsattype.Location = New System.Drawing.Point(153, 198)
+        Me.ComboAnsattype.Name = "ComboAnsattype"
+        Me.ComboAnsattype.Size = New System.Drawing.Size(121, 21)
+        Me.ComboAnsattype.TabIndex = 8
+        '
+        'btnRegistrerBruker
+        '
+        Me.btnRegistrerBruker.Location = New System.Drawing.Point(153, 240)
+        Me.btnRegistrerBruker.Name = "btnRegistrerBruker"
+        Me.btnRegistrerBruker.Size = New System.Drawing.Size(122, 23)
+        Me.btnRegistrerBruker.TabIndex = 9
+        Me.btnRegistrerBruker.Text = "Registrer ny bruker"
+        Me.btnRegistrerBruker.UseVisualStyleBackColor = True
+        '
+        'RegistrerBruker
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(323, 296)
+        Me.Controls.Add(Me.btnRegistrerBruker)
+        Me.Controls.Add(Me.ComboAnsattype)
+        Me.Controls.Add(Me.txtgjentapw)
+        Me.Controls.Add(Me.txtregistrerpw)
+        Me.Controls.Add(Me.txtRegistrerbrukernavn)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "RegistrerBruker"
         Me.Text = "RegistrerBruker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtRegistrerbrukernavn As TextBox
+    Friend WithEvents txtregistrerpw As TextBox
+    Friend WithEvents txtgjentapw As TextBox
+    Friend WithEvents ComboAnsattype As ComboBox
+    Friend WithEvents btnRegistrerBruker As Button
 End Class
--- TurCareASOOPS/TurCareASOOPS/RegistrerBruker.vb
+++ TurCareASOOPS/TurCareASOOPS/RegistrerBruker.vb
@@ -1,3 +1,44 @@
-﻿Public Class RegistrerBruker
+﻿Imports MySql.Data.MySqlClient
 
+Public Class RegistrerBruker
+
+    Public tilkobling = New MySqlConnection("Server=mysql.stud.iie.ntnu.no;Database=perkp;Uid=perkp;Pwd=passord123")
+
+    Private Function sporring(ByVal sql As String) As DataTable
+        Dim mydata As New DataTable
+        Try
+            tilkobling.Open()
+            Dim kommando As New MySqlCommand(sql, tilkobling)
+            Dim da As New MySqlDataAdapter
+            da.SelectCommand = kommando
+            da.Fill(mydata)
+            tilkobling.Close()
+        Catch ex As Exception
+            MessageBox.Show("Noe gikk galt. " & ex.Message)
+            tilkobling.Close()
+        End Try
+        Return mydata
+
+    End Function
+
+    Private Sub btnRegistrerBruker_Click(sender As Object, e As EventArgs) Handles btnRegistrerBruker.Click
+
+        Dim brukernavn As String = txtRegistrerbrukernavn.Text
+        Dim passord As String = txtregistrerpw.Text
+        Dim passord2 As String = txtgjentapw.Text
+        Dim typeansatt As String = ComboAnsattype.Text
+        If passord = passord2 Then
+
+            sporring("INSERT INTO brukere (brukernavn, passord, type) VALUES ('" & brukernavn & "', '" & passord & "', '" & typeansatt & "');")
+            MessageBox.Show("Brukeren ble lagt til i databasen")
+            txtRegistrerbrukernavn.Clear()
+            txtregistrerpw.Clear()
+            txtgjentapw.Clear()
+            ComboAnsattype.ResetText()
+
+        Else
+            MessageBox.Show("Passordene er ikke like")
+        End If
+
+    End Sub
 End Class
