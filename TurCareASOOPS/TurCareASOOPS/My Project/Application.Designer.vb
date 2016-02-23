52e66f6be316c6c232b4e2878232a6a0d4a067d2
Slawomir Banach <slawomib@student.hist.no>
2016-02-23 12:48:07 +0100

    login database, login ansatt

--- TurCareASOOPS/TurCareASOOPS/My Project/Application.Designer.vb
+++ TurCareASOOPS/TurCareASOOPS/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TurCareASOOPS.Form1
+            Me.MainForm = Global.TurCareASOOPS.LoginDB
         End Sub
     End Class
 End Namespace
