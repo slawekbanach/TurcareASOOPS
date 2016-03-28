8efb7d66569c59cc3e9a2d5ef0c0c38cfa47d6ed
Slawomir Banach <slawomib@student.hist.no>
2016-03-28 19:51:34 +0200

    mekka salg sånn ish

--- TurCareASOOPS/TurCareASOOPS/RegistrerSalg.Designer.vb
+++ TurCareASOOPS/TurCareASOOPS/RegistrerSalg.Designer.vb
@@ -22,91 +22,188 @@ Partial Class RegistrerSalg
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtSelger = New System.Windows.Forms.TextBox()
+        Me.txtKunde = New System.Windows.Forms.TextBox()
+        Me.txtDato = New System.Windows.Forms.TextBox()
+        Me.txtVare = New System.Windows.Forms.TextBox()
+        Me.txtPris = New System.Windows.Forms.TextBox()
+        Me.btnRegistrerSalg = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnVarer = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Pris = New System.Windows.Forms.Label()
+        Me.txtAntall = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnKunder = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'txtSelger
         '
-        Me.TextBox1.Location = New System.Drawing.Point(178, 84)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(181, 26)
-        Me.TextBox1.TabIndex = 0
+        Me.txtSelger.Location = New System.Drawing.Point(154, 67)
+        Me.txtSelger.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtSelger.Name = "txtSelger"
+        Me.txtSelger.Size = New System.Drawing.Size(122, 20)
+        Me.txtSelger.TabIndex = 0
         '
-        'TextBox2
+        'txtKunde
         '
-        Me.TextBox2.Location = New System.Drawing.Point(178, 140)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(181, 26)
-        Me.TextBox2.TabIndex = 1
+        Me.txtKunde.Location = New System.Drawing.Point(154, 103)
+        Me.txtKunde.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtKunde.Name = "txtKunde"
+        Me.txtKunde.Size = New System.Drawing.Size(122, 20)
+        Me.txtKunde.TabIndex = 1
         '
-        'TextBox3
+        'txtDato
         '
-        Me.TextBox3.Location = New System.Drawing.Point(178, 197)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(181, 26)
-        Me.TextBox3.TabIndex = 2
+        Me.txtDato.Location = New System.Drawing.Point(154, 140)
+        Me.txtDato.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtDato.Name = "txtDato"
+        Me.txtDato.Size = New System.Drawing.Size(122, 20)
+        Me.txtDato.TabIndex = 2
         '
-        'TextBox4
+        'txtVare
         '
-        Me.TextBox4.Location = New System.Drawing.Point(178, 254)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(181, 26)
-        Me.TextBox4.TabIndex = 3
+        Me.txtVare.Location = New System.Drawing.Point(154, 177)
+        Me.txtVare.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtVare.Name = "txtVare"
+        Me.txtVare.Size = New System.Drawing.Size(122, 20)
+        Me.txtVare.TabIndex = 3
         '
-        'TextBox5
+        'txtPris
         '
-        Me.TextBox5.Location = New System.Drawing.Point(178, 306)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(181, 26)
-        Me.TextBox5.TabIndex = 4
+        Me.txtPris.Location = New System.Drawing.Point(154, 247)
+        Me.txtPris.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.txtPris.Name = "txtPris"
+        Me.txtPris.Size = New System.Drawing.Size(122, 20)
+        Me.txtPris.TabIndex = 4
         '
-        'Button1
+        'btnRegistrerSalg
         '
-        Me.Button1.Location = New System.Drawing.Point(178, 358)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(181, 43)
-        Me.Button1.TabIndex = 5
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnRegistrerSalg.Location = New System.Drawing.Point(154, 281)
+        Me.btnRegistrerSalg.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnRegistrerSalg.Name = "btnRegistrerSalg"
+        Me.btnRegistrerSalg.Size = New System.Drawing.Size(121, 28)
+        Me.btnRegistrerSalg.TabIndex = 5
+        Me.btnRegistrerSalg.Text = "Registrer Salg"
+        Me.btnRegistrerSalg.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(12, 377)
+        Me.Button2.Location = New System.Drawing.Point(11, 353)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(113, 48)
+        Me.Button2.Size = New System.Drawing.Size(75, 31)
         Me.Button2.TabIndex = 6
         Me.Button2.Text = "<- Tilbake"
         Me.Button2.UseVisualStyleBackColor = True
         '
-        'Button3
+        'btnVarer
         '
-        Me.Button3.Location = New System.Drawing.Point(78, 187)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 36)
-        Me.Button3.TabIndex = 7
-        Me.Button3.Text = "Button3"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnVarer.Location = New System.Drawing.Point(119, 353)
+        Me.btnVarer.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.btnVarer.Name = "btnVarer"
+        Me.btnVarer.Size = New System.Drawing.Size(123, 31)
+        Me.btnVarer.TabIndex = 7
+        Me.btnVarer.Text = "Oversikt over varer"
+        Me.btnVarer.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(77, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(37, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Selger"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(77, 106)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(38, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Kunde"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(77, 143)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Salgsdato"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(77, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(29, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Vare"
+        '
+        'Pris
+        '
+        Me.Pris.AutoSize = True
+        Me.Pris.Location = New System.Drawing.Point(82, 250)
+        Me.Pris.Name = "Pris"
+        Me.Pris.Size = New System.Drawing.Size(24, 13)
+        Me.Pris.TabIndex = 12
+        Me.Pris.Text = "Pris"
+        '
+        'txtAntall
+        '
+        Me.txtAntall.Location = New System.Drawing.Point(154, 211)
+        Me.txtAntall.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtAntall.Name = "txtAntall"
+        Me.txtAntall.Size = New System.Drawing.Size(122, 20)
+        Me.txtAntall.TabIndex = 13
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(77, 214)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(33, 13)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Antall"
+        '
+        'btnKunder
+        '
+        Me.btnKunder.Location = New System.Drawing.Point(269, 353)
+        Me.btnKunder.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnKunder.Name = "btnKunder"
+        Me.btnKunder.Size = New System.Drawing.Size(123, 31)
+        Me.btnKunder.TabIndex = 15
+        Me.btnKunder.Text = "Oversikt over kunder"
+        Me.btnKunder.UseVisualStyleBackColor = True
         '
         'RegistrerSalg
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(524, 437)
-        Me.Controls.Add(Me.Button3)
+        Me.ClientSize = New System.Drawing.Size(403, 395)
+        Me.Controls.Add(Me.btnKunder)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtAntall)
+        Me.Controls.Add(Me.Pris)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnVarer)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnRegistrerSalg)
+        Me.Controls.Add(Me.txtPris)
+        Me.Controls.Add(Me.txtVare)
+        Me.Controls.Add(Me.txtDato)
+        Me.Controls.Add(Me.txtKunde)
+        Me.Controls.Add(Me.txtSelger)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "RegistrerSalg"
         Me.Text = "RegistrerSalg"
         Me.ResumeLayout(False)
@@ -114,12 +211,20 @@ Partial Class RegistrerSalg
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents txtSelger As TextBox
+    Friend WithEvents txtKunde As TextBox
+    Friend WithEvents txtDato As TextBox
+    Friend WithEvents txtVare As TextBox
+    Friend WithEvents txtPris As TextBox
+    Friend WithEvents btnRegistrerSalg As Button
     Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents btnVarer As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Pris As Label
+    Friend WithEvents txtAntall As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnKunder As Button
 End Class
--- TurCareASOOPS/TurCareASOOPS/RegistrerSalg.vb
+++ TurCareASOOPS/TurCareASOOPS/RegistrerSalg.vb
@@ -1,11 +1,47 @@
-﻿Public Class RegistrerSalg
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim selgerid As String = TextBox1.Text
-        Dim kundeid As String = TextBox2.Text
-        Dim dato As String = TextBox3.Text
-        Dim vare As String = TextBox4.Text
-        Dim pris As String = TextBox5.Text
+﻿Imports MySql.Data.MySqlClient
+Public Class RegistrerSalg
+    Public tilkobling = New MySqlConnection("Server=mysql.stud.iie.ntnu.no;Database=g_oops_t4;Uid=g_oops_t4;Pwd=passord123")
+    Private Function sporring(ByVal sql As String) As DataTable
+        Dim mydata As New DataTable
+        Try
+            tilkobling.Open()
+            Dim kommando As New MySqlCommand(sql, tilkobling)
+            Dim da As New MySqlDataAdapter
+            da.SelectCommand = kommando
+            da.Fill(mydata)
+            tilkobling.Close()
+        Catch ex As Exception
+            MessageBox.Show("Noe gikk galt. " & ex.Message)
+            tilkobling.Close()
+        End Try
+        Return mydata
 
+    End Function
+
+    Private Sub RegistrerSalg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtSelger.Text = LoginUser.bruker
+        txtSelger.Enabled = False
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnRegistrerSalg.Click
+        Dim selgerid As String = txtSelger.Text
+        Dim kundeid As String = txtKunde.Text
+        Dim dato As String = txtDato.Text
+        Dim vare As String = txtVare.Text
+        Dim pris As String = txtPris.Text
+        Dim antall As String = txtAntall.Text
+        Try
+            sporring("INSERT INTO salg (salg_selger_id, salg_kunde_id, salg_dato, salg_vare, salg_antall, salg_pris) VALUES ('" & selgerid & "', '" & kundeid & "', '" & dato & "', '" & vare & "', '" & antall & "', '" & pris & "');")
+            MessageBox.Show("Registrering av salg vellykket!")
+            txtKunde.Text = ""
+            txtDato.Text = ""
+            txtVare.Text = ""
+            txtPris.Text = ""
+            txtAntall.Text = ""
+        Catch ex As Exception
+            MessageBox.Show("Feil: " & ex.Message)
+        End Try
 
 
     End Sub
@@ -16,11 +52,13 @@
 
     End Sub
 
-    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
 
+
+    Private Sub btnVarer_click(sender As Object, e As EventArgs) Handles btnVarer.Click
+        SjekkLager.Show()
     End Sub
 
-    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        MessageBox.Show(LoginUser.bruker)
+    Private Sub btnKunder_click(sender As Object, e As EventArgs) Handles btnKunder.Click
+        ListUtKunder.Show()
     End Sub
 End Class
